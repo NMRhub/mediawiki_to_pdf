b344d4d6eef04955b6515625ae767d44981739d5
Gerard Weatherby <gweatherby@uchc.edu>
2025-11-17 09:01:30 -0500

    Version text files instead of replacing

--- src/mediawiki_to_pdf/box.py
+++ src/mediawiki_to_pdf/box.py
@@ -6,9 +6,19 @@ from boxsdk import OAuth2, Client
 
 from mediawiki_to_pdf import mediawiki_to_pdf_logger
 
+DEFAULT_MAP = {'pdf': False, 'txt': True}
 
-def upload(config, files: Iterable[Path]):
-    """Upload files to box using information from config"""
+
+def _find_matches(output: set, ext: str, source: Iterable[Path]) -> None:
+    for file in source:
+        if file.name.endswith(ext):
+            output.add(file)
+
+
+def upload(config, files: Iterable[Path], disposition_map: dict = None):
+    """Upload files to box using information from config
+    disposition_map > True to upload new version, False to replace file"""
+    tmap = disposition_map if disposition_map is not None else DEFAULT_MAP
     id = config['client_id']
     secret = config['secret']
     folder_id = str(config['folder'])
@@ -34,11 +44,44 @@ def upload(config, files: Iterable[Path]):
     mediawiki_to_pdf_logger.info(f"Acting as: {user.id}  {user.name}")
 
     folder = client.folder(folder_id=folder_id)
-    new_names = set(f.name for f in files)
-    for item in folder.get_items():
-        if item.name in new_names:
-            mediawiki_to_pdf_logger.info(f"Deleting existing {item.name}")
-            item.delete()
-    for file in files:
+    replacement_files = set()
+    versioned_files = set()
+    for extension, versioned in tmap.items():
+        if versioned:
+            _find_matches(versioned_files, extension, files)
+        else:
+            _find_matches(replacement_files, extension, files)
+    unmapped = set(files) - replacement_files - versioned_files
+    if unmapped:
+        bad_names = ','.join(u.name for u in unmapped)
+        raise ValueError(f"Unmapped extensions on {bad_names}")
+
+    new_names = set(f.name for f in replacement_files)
+    FETCH_LIMIT = 1000
+    # noinspection PyUnresolvedReferences
+    existing = {item.name: item for item in folder.get_items(limit=FETCH_LIMIT)}
+    if len(existing) >= FETCH_LIMIT:
+        raise ValueError(f"Only {FETCH_LIMIT} items in folder supported")
+
+    for name in existing.keys():
+        if name in new_names:
+            mediawiki_to_pdf_logger.info(f"Deleting existing {name}")
+            existing[name].delete()
+    for file in replacement_files:
         uploaded_file = folder.upload(file.as_posix(), file.name)
         mediawiki_to_pdf_logger.info(f"Uploaded {uploaded_file}")
+
+    for file in versioned_files:
+        fname = file.name
+        if fname in existing:
+            # File exists → upload a new version
+            mediawiki_to_pdf_logger.info(f"Updating version of {fname}")
+            box_file = existing[fname]
+            # noinspection PyUnresolvedReferences
+            uploaded = box_file.update_contents(file.as_posix())
+        else:
+            # File does not exist → normal create
+            mediawiki_to_pdf_logger.info(f"Uploading new file {fname}")
+            uploaded = folder.upload(file.as_posix(), fname)
+
+        mediawiki_to_pdf_logger.info(f"Uploaded: {uploaded}")
